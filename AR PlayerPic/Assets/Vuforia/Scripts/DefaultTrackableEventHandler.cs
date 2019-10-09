6653a530a67718e8cada399574d30e1f0f8cb207
Bram <bghvanvugt@gmail.com>
2019-10-09 13:50:43 +0200

    speler loopt uit poster
    Wanneer de poster gescant wordt loopt de speler uit de poster en gaat stil staan.

--- AR PlayerPic/Assets/Vuforia/Scripts/DefaultTrackableEventHandler.cs
+++ AR PlayerPic/Assets/Vuforia/Scripts/DefaultTrackableEventHandler.cs
@@ -17,6 +17,8 @@ using Vuforia;
 /// </summary>
 public class DefaultTrackableEventHandler : MonoBehaviour, ITrackableEventHandler
 {
+
+
     #region PROTECTED_MEMBER_VARIABLES
 
     protected TrackableBehaviour mTrackableBehaviour;
@@ -98,6 +100,7 @@ public class DefaultTrackableEventHandler : MonoBehaviour, ITrackableEventHandle
         // Enable canvas':
         foreach (var component in canvasComponents)
             component.enabled = true;
+        gameObject.transform.GetChild(0).gameObject.SetActive(true);
     }
 
 
